14afd398f8b0e307071662654dbfa0c9abe85dae
sid-mandati <siddhartha.mandati@gmail.com>
2017-06-23 15:47:16 -0500

    added printlns to debug

--- src/main/scala/com/jenkinstest/JenkinsTestScala.scala
+++ src/main/scala/com/jenkinstest/JenkinsTestScala.scala
@@ -49,27 +49,31 @@ import scala.collection.immutable
 
 object JenkinsTestScala {
   def main(args: Array[String]){
+    println("1")
     System.setProperty("user.timezone", "UTC")
     //println("ETL started at " + new DateTime())
+    println("2")
     val etl_start_time = new DateTime()
-   
+   println("3")
     
     val sparkConf = new SparkConf().setAppName("Reno_ETL")
     .setMaster("local[4]")
-    
+    println("4")
     val sc = new SparkContext(sparkConf)
-    
+    println("4")
     sc.setLogLevel("ERROR")
-    
+    println("5")
     val sqlContext = new SQLContext(sc) 
+    println("6")
     val mongoIP = "127.0.0.11:27017"
-    
+    println("7")
     //val mongoClient = MongoClient("30.0.1.142", 27017)
     val mongoClient = MongoClient("127.0.0.11", 27017)
-    
+    println("8")
     val etlConfigDBName = "etl_config"
+    println("9")
     val etlConfigCollName = "reno_etl_config"
-    
+    println("10")
     val etlConfigConnectionURI = "mongodb://"+mongoIP+"/"+etlConfigDBName
     
     
