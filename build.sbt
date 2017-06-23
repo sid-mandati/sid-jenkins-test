b1e03a346113c5d972c8fbdb6d0daa9429e70021
sid-mandati <siddhartha.mandati@gmail.com>
2017-06-23 14:30:58 -0500

    Changed to test mongodb connection

--- build.sbt
+++ build.sbt
@@ -4,10 +4,31 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0" 
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.1.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.0"
 
-mainClass in Compile := Some("com.jenkinstest.JenkinsTestScala")
 
+ libraryDependencies += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.5"
+ libraryDependencies += "com.fasterxml.jackson.core" % "jackson-annotations" % "2.8.5"
+ libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.5"
+ libraryDependencies += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.5"
+ libraryDependencies += "org.json4s" % "json4s-ast_2.11" % "3.2.11"
+ libraryDependencies += "org.json4s" % "json4s-core_2.11" % "3.2.11"
+ libraryDependencies += "org.json4s" % "json4s-jackson_2.11" % "3.2.11"
+ libraryDependencies += "joda-time" % "joda-time" % "2.9.3"
+ libraryDependencies += "org.jodd" % "jodd-core" % "3.5.2"
+
+ libraryDependencies += "org.mongodb" % "casbah-commons_2.11" % "3.0.0"
+ libraryDependencies += "org.mongodb" % "casbah-core_2.11" % "3.0.0"
+ libraryDependencies += "org.mongodb" % "casbah-query_2.11" % "3.0.0"
+ libraryDependencies += "com.typesafe.play" % "play-json_2.11" % "2.6.0-M2"
+ libraryDependencies += "com.typesafe.play" % "play-functional_2.11" % "2.6.0-M2"
+ libraryDependencies += "org.mongodb.spark" % "mongo-spark-connector_2.11" % "2.0.0"
+ libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.1.0"
+ libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % "2.1.0"
+ 
+ 
  assemblyMergeStrategy in assembly := {
  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
  case x => MergeStrategy.first
--- src/main/scala/com/jenkinstest/JenkinsTestScala.scala
+++ src/main/scala/com/jenkinstest/JenkinsTestScala.scala
@@ -1,7 +1,85 @@
 package com.jenkinstest
 
+
+import org.apache.spark.streaming._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf,SparkContext}
+
+import play.api.libs.json._
+import play.api.libs.functional._
+import org.apache.spark.sql.{SQLContext,Row,Column,DataFrame}
+import java.util.HashMap
+
+import scala.collection.mutable.ArrayBuffer
+import org.apache.spark.sql.functions.{unix_timestamp, to_date}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.functions._
+
+import java.util.Calendar
+import java.text.SimpleDateFormat 
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.DefaultFormats
+
+import com.mongodb.spark._
+import com.mongodb.spark.sql._
+import com.mongodb.spark.config._
+
+import org.joda.time._
+import org.joda.time.format._
+
+import com.mongodb.casbah._
+
+import com.mongodb.casbah.MongoClient
+import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.casbah.commons.MongoDBList
+import com.mongodb.casbah.query.Imports._
+
+import com.mongodb.DBObject
+import com.mongodb.casbah.MongoClientURI
+import com.mongodb.util.JSON
+
+import com.mongodb.casbah.commons.conversions.scala._ 
+import scala.collection.immutable
+
+
 object JenkinsTestScala {
   def main(args: Array[String]){
-    println("Testing commits")
+    System.setProperty("user.timezone", "UTC")
+    //println("ETL started at " + new DateTime())
+    val etl_start_time = new DateTime()
+   
+    
+    val sparkConf = new SparkConf().setAppName("Reno_ETL")
+    //.setMaster("local[4]")
+    
+    val sc = new SparkContext(sparkConf)
+    
+    sc.setLogLevel("ERROR")
+    
+    val sqlContext = new SQLContext(sc) 
+    val mongoIP = "127.0.0.11:27017"
+    
+    //val mongoClient = MongoClient("30.0.1.142", 27017)
+    val mongoClient = MongoClient("127.0.0.11", 27017)
+    
+    val etlConfigDBName = "etl_config"
+    val etlConfigCollName = "reno_etl_config"
+    
+    val etlConfigConnectionURI = "mongodb://"+mongoIP+"/"+etlConfigDBName
+    
+    
+    //Get Config Details
+    val etlConfigDf = sqlContext.read.format("com.mongodb.spark.sql.DefaultSource")
+                    .option("spark.mongodb.input.uri", etlConfigConnectionURI)
+                    .option("spark.mongodb.input.collection", etlConfigCollName)
+                    .load()
+                    
+    val testingprint = etlConfigDf.select("ods_db_name").head().getString(0)
+    println("test String is " + testingprint)
   }
 }
