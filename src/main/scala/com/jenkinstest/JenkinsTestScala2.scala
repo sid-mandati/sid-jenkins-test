f45cb9cdf4796602dd668f3a49da3a05af26e53c
sid-mandati <siddhartha.mandati@gmail.com>
2017-06-26 11:13:48 -0500

    Dummy program

---
+++ src/main/scala/com/jenkinstest/JenkinsTestScala2.scala
@@ -0,0 +1,26 @@
+package com.jenkinstest
+
+
+
+import org.apache.spark.{SparkConf,SparkContext}
+
+import org.apache.spark.sql.{SQLContext,Row,Column,DataFrame}
+
+
+import org.joda.time._
+import org.joda.time.format._
+
+import com.mongodb.casbah._
+
+import com.mongodb.DBObject
+import com.mongodb.casbah.MongoClientURI
+import com.mongodb.util.JSON
+
+
+
+object JenkinsTestScala2 {
+  def main(args: Array[String]){
+    println("Im here")
+
+  }
+}
